7e699793260cdd71ae80faa514aa3f4b5fe93763
Anuj Singh <anuj.s@mindfiresolutions.com>
2024-10-28 19:26:02 +0530

    wrong message format

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,9 +1,4 @@
 export default {
-    '**/*.{js,jsx,ts,tsx}': [
-        'eslint --fix',
-        'prettier --write'
-    ],
-    '**/*.{json,md}': 'prettier --write',
+  '**/*.{js,jsx,ts,tsx}': ['eslint --fix', 'prettier --write'],
+  '**/*.{json,md}': 'prettier --write',
 };
-
-
